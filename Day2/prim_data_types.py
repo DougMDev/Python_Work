9f917806c9be7596eb876d5eb67ab1a147163790
Doug <dougmdev@outlook.com>
2022-06-18 19:30:15 +0100

    primitive data types file

---
+++ Day2/prim_data_types.py
@@ -0,0 +1,21 @@
+from pickle import TRUE
+
+
+print(len("Hello"))
+
+#if you print length of number you get an error,
+#print(len(12345))
+
+a = "Doug"
+b = 245
+c = True
+
+print(123 + 456) #Added as a Math Problem
+print("123" + "456") #Put together as a String
+
+#num_char = len(input("What is your Name? "))
+#print("Your name has + " + num_char + " Characters")
+#print("Your name has " + str(num_char) + " Characters") #Forces the int to be a Str
+#print(type(num_char))
+
+print(70 + int(105))
