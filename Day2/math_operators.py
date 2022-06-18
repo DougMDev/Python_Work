7fa4c370656de016777a0683c5d4c30e6658cd83
Doug <dougmdev@outlook.com>
2022-06-18 20:14:47 +0100

    exercises all done

---
+++ Day2/math_operators.py
@@ -0,0 +1,15 @@
+a = 3 + 5
+b = 3 - 5
+c = 3 * 5
+d = 3 / 5 #always returns a float
+e = 8 ** 2 #squared
+
+#PEMDAS
+
+# ()
+# **
+# * /
+# + -
+
+print(3 * 3 + 3 / 3 - 3) #7
+print(3 * (3 + 3) / 3 -3) #3
---
+++ Day2/number_manipulation.py
@@ -0,0 +1,18 @@
+print(8 / 3) #chops decimals
+print(round(8 / 3)) #rounds to whole number
+print(round(8 / 3, 2)) #rounds to 2 decimal places
+print(round(8 / 3, 15)) #rounds to 15 decimal places
+
+print( 8 // 3) # Floor function returns an integar (2)
+
+score = 0
+score += 5
+score += 10
+height = 1.9
+isWinning = True
+
+print(score) #15
+
+print("Your Score Is " + str(score))
+#f-string
+print(f"Your Score is {score}, your height is {height}, and you are winning is : {isWinning}")
