99a83572464387694f69357069b293281457ca3c
Doug <dougmdev@outlook.com>
2022-06-19 18:28:58 +0100

    Initialise Day 3

---
+++ Day3/basic_ifElse.py
@@ -0,0 +1,16 @@
+water_level = 70
+
+if water_level > 80:
+    print("The water has been drained")
+else:
+    print("keep filling")
+
+
+
+print("Welcome to the Rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can purchase a ticket! Go Ahead!")
+else:
+    print("Sorry, you are too small!")
