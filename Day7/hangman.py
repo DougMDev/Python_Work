28cba860833ebfc4bdd7a8eddda782d1e9d6756d
Doug <dougmdev@outlook.com>
2022-06-25 22:51:50 +0100

    While loop for game status

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -1,26 +1,34 @@
 #Step 1 
+from pickle import FALSE
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
 # rand_int = random.randint(0, 2)
 # rand_word = word_list[rand_int]
 rand_word = random.choice(word_list)
+word_len = len(rand_word)
 
 #Testing code
 print(f'Pssst, the solution is {rand_word}.')
 
-#TO DO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
-guess = input("Guess a Letter! ").lower()
-
-#TO DO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-
+display = []
 for letter in rand_word:
-    if letter == guess:
-        print("Correct")
-    else:
-        print("Wrong")
+    display.append("_")
+print(display)
+
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Guess a Letter! ").lower()
+
+    #Check guessed letter
+    for position in range(word_len):
+        letter = rand_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+if "_" not in display:
+    end_of_game = True
+    print("You WIN!!!")
