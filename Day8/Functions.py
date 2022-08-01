45a85b8eb4355b0a37fd0e3e838bde61ff78c74f
Doug <dougmdev@outlook.com>
2022-08-01 22:53:32 +0100

    Day 8 of 100 Days of Code Complete

---
+++ Day8/Functions.py
@@ -0,0 +1,8 @@
+def greet(name):
+    print(f"Welcome {name}!")
+    print(f"How are you doing today {name}?")
+    print("Im here to help ! :)")
+
+name = input("What is your name?? ")
+
+greet(name)
---
+++ Day8/MoreInputs.py
@@ -0,0 +1,11 @@
+def doubleParam(name, location):
+    print(f"Hello {name} from {location}")
+    print(f"We can see it is nice there!")
+    print(f"Hope you are doing well {name}")
+
+name = input("What is your name? ")
+location = input("Where are you currently based? ")
+
+# doubleParam("Doug", "Frome")
+
+doubleParam(name, location)
